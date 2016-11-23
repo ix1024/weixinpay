1e94414cdf94b2b79a7bdba6ff70372b00a06e11
lengjh <jinhua.leng@gmail.com>
2016-11-23 18:07:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,224 +1,192 @@
 'use strict';
-/**
- * =====微信支付相关功能=====
- */
-
-var crypto = require('crypto');
+var md5 = require('md5');
 var request = require('request');
-var utils = require('npm-utils-kingwell');
+var xml2js = require('xml2js');
+var parseString = xml2js.parseString;
+var builder = new xml2js.Builder();
 
 /**
- * 获取随机字符串
- * @return {String} string
+ * @description Extension method add
+ * @param  {String} name  property
+ * @param  {Object Or String} value property value
+ * @return {Object}       function
  */
-var createNonceStr = function() {
-	return Math.random().toString(36).substr(2, 15);
+Function.prototype.add = Function.prototype.add || function(name, value) {
+	this.prototype[name] = value;
+	return this;
 };
-
 
 /**
- * 获取时间戳
- * @return {[Number]} 
+ * @description Extension method inherits
+ * @param  {Object} superConstructor superConstructor
+ * @return {Object}                  function
  */
-var createTimeStamp = function() {
-	return parseInt(new Date().getTime() / 1000) + '';
-};
-
-
-/**
- * 创建统一下单
- * @constructor Unifiedorder
- * @param {Object} [varname] [description]
- */
-var Unifiedorder = function() {
-	this.init.apply(this, arguments);
-};
-var fn = Unifiedorder.prototype;
-fn.init = function(options) {
-	var ops = options || {};
-	for (var key in ops) {
-		this[key] = ops[key];
+Function.prototype.inherits = Function.prototype.inherits || function(superConstructor) {
+	var arr = arguments;
+	if (Array.isArray(arguments[0]) && arguments.length === 1) {
+		arr = arguments[0];
 	}
-	this.order();
-};
-/**
- * 统一下单URL
- * @type {String}
- */
-fn.url = 'https://api.mch.weixin.qq.com/pay/unifiedorder';
-fn.createTimeStamp = createTimeStamp;
-fn.createNonceStr = createNonceStr;
-fn.signType = "MD5";
-fn.complete = function() {};
-/**
- * get xml value
- * @return {String}
- */
-fn.getXMLNodeValue = function(node_name, xml) {
-	try {
-		var tmp = xml.split("<" + node_name + ">");
-		var _tmp = tmp[1].split("</" + node_name + ">");
-		return _tmp[0];
-	} catch (ev) {
-		return '';
-	}
-};
-
-
-/**
- * Join Object to String
- * @return {[type]} [description]
- */
-fn.raw = function(args) {
-	var keys = Object.keys(args);
-	var newArgs = {};
-	var string = '';
-	keys = keys.sort()
-
-	keys.forEach(function(key) {
-		newArgs[key] = args[key];
-	});
-
-	for (var k in newArgs) {
-		string += '&' + k + '=' + newArgs[k];
-	}
-	string = string.substr(1);
-	return string;
-};
-
-/**
- * [paysignjs description]
- * @param  {[type]} appid     [description]
- * @param  {[type]} nonceStr  [description]
- * @param  {[type]} packages  [description]
- * @param  {[type]} signType  [description]
- * @param  {[type]} timeStamp [description]
- * @return {[type]}           [description]
- */
-fn.paysignjs = function(appid, nonceStr, packages, signType, timeStamp) {
-	var that = this;
-	var ret = {
-		appId: appid,
-		nonceStr: nonceStr,
-		package: packages,
-		signType: signType,
-		timeStamp: timeStamp
-	};
-	var string = this.raw(ret);
-	string = string + '&key=' + that.key;
-	var sign = crypto.createHash('md5').update(string, 'utf8').digest('hex');
-	return sign.toUpperCase();
-};
-/**
- * [paysignjsapi description]
- * @param  {[type]} appid            [description]
- * @param  {[type]} attach           [description]
- * @param  {[type]} body             [description]
- * @param  {[type]} mch_id           [description]
- * @param  {[type]} nonce_str        [description]
- * @param  {[type]} notify_url       [description]
- * @param  {[type]} openid           [description]
- * @param  {[type]} out_trade_no     [description]
- * @param  {[type]} spbill_create_ip [description]
- * @param  {[type]} total_fee        [description]
- * @param  {[type]} trade_type       [description]
- * @return {[type]}                  [description]
- */
-fn.paysignjsapi = function(appid, attach, body, mch_id, nonce_str, notify_url, openid, out_trade_no, spbill_create_ip, total_fee, trade_type) {
-	var ret = {
-		appid: appid,
-		attach: attach,
-		body: body,
-		mch_id: mch_id,
-		nonce_str: nonce_str,
-		notify_url: notify_url,
-		openid: openid,
-		out_trade_no: out_trade_no,
-		spbill_create_ip: spbill_create_ip,
-		total_fee: total_fee,
-		trade_type: trade_type
-	};
-	var string = this.raw(ret);
-	string = string + '&key=' + this.key; //key为在微信商户平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置  
-	var crypto = require('crypto');
-	var sign = crypto.createHash('md5').update(string, 'utf8').digest('hex');
-	return sign.toUpperCase();
-};
-
-// 此处的attach不能为空值 否则微信提示签名错误 
-fn.order = function() {
-	var that = this;
-	//var deferred = Q.defer();
-	//var appid = config.member_config.appid;
-	var nonce_str = that.createNonceStr();
-	var timeStamp = that.createTimeStamp();
-	var formData = "<xml>";
-	formData += "<appid>" + that.appid + "</appid>"; //appid  
-	formData += "<attach>" + that.attach + "</attach>"; //附加数据  
-	formData += "<body>" + that.body + "</body>";
-	formData += "<mch_id>" + that.mch_id + "</mch_id>"; //商户号  
-	formData += "<nonce_str>" + nonce_str + "</nonce_str>"; //随机字符串，不长于32位。  
-	formData += "<notify_url>" + that.notify_url + "</notify_url>";
-	formData += "<openid>" + that.openid + "</openid>";
-	formData += "<out_trade_no>" + that.bookingNo + "</out_trade_no>";
-	formData += "<spbill_create_ip>" + that.ip + "</spbill_create_ip>";
-	formData += "<total_fee>" + that.total_fee + "</total_fee>";
-	formData += "<trade_type>" + that.trade_type + "</trade_type>";
-	formData += "<sign>" + this.paysignjsapi(that.appid, that.attach, that.body, that.mch_id, nonce_str, that.notify_url, that.openid, that.bookingNo, that.ip, that.total_fee, that.trade_type) + "</sign>";
-	formData += "</xml>";
-
-
-	request({
-		url: that.url,
-		method: 'POST',
-		body: formData
-	}, function(err, response, body) {
-		var body = body.toString('utf-8') || '';
-		var getCodeText = function(str) {
-			return str.slice(9, -3);
-		};
-		var result = {
-			return_code: getCodeText(that.getXMLNodeValue('return_code', body)),
-			err_code: getCodeText(that.getXMLNodeValue('err_code', body)),
-			err_code_des: getCodeText(that.getXMLNodeValue('err_code_des', body)),
-			return_msg: getCodeText(that.getXMLNodeValue('return_msg', body))
-		};
-
-		console.log(body);
-		console.log(222222222, that.complete.toString())
-
-		if (!err &&
-			response.statusCode == 200 &&
-			result.return_code.toUpperCase() !== 'FAIL' &&
-			!result.err_code_des
-		) {
-
-			var prepay_id = that.getXMLNodeValue('prepay_id', body.toString("utf-8")) || '';
-			var tmp = prepay_id.split('[');
-			var tmp1 = tmp[2].split(']');
-			//签名  
-			var _paySignjs = that.paysignjs(that.appid, nonce_str, 'prepay_id=' + tmp1[0], that.signType, timeStamp);
-
-			result.appId = that.appid;
-			result.timeStamp = timeStamp;
-			result.nonceStr = nonce_str;
-			result.signType = that.signType;
-			result.package = tmp1[0];
-			result.paySign = _paySignjs;
-			that.complete(response, result);
-
-		} else {
-
-			that.complete(response, result);
-
+	for (var item = arr.length; item >= 0; item--) {
+		for (var key in arr[item]) {
+			this.prototype[key] = arr[item][key];
 		}
+	}
+	return this;
+};
+
+/**
+ * @constructor WXPayUrl
+ * @description微信支付URL
+ */
+function WXPayUrl() {}
+WXPayUrl.add('url', {
+	unifiedorder: 'https://api.mch.weixin.qq.com/pay/unifiedorder', //统一下单
+	orderquery: 'https://api.mch.weixin.qq.com/pay/orderquery', //查询订单
+	closeorder: 'https://api.mch.weixin.qq.com/pay/closeorder', //关闭订单
+	refund: 'https://api.mch.weixin.qq.com/pay/refund', //申请退款
+	refundquery: 'https://api.mch.weixin.qq.com/pay/refundquery', //查询退款
+	downloadbill: 'https://api.mch.weixin.qq.com/pay/downloadbill', //下载对账单
+	report: 'https://api.mch.weixin.qq.com/pay/report' //交易保障
+});
+/**
+ * @description 微信支付工具类函数
+ */
+function Utils() {
+	this.init = function() {};
+}
+Utils
+	.add('options', {})
+	.add('parseWXReturnXML', function(xmlObject) {
+		var newObject = {};
+		xmlObject = xmlObject.xml || {};
+		for (var key in xmlObject) {
+			newObject[key] = xmlObject[key][0];
+		}
+		return newObject;
+	})
+	.add('createNonceStr', function() {
+		return Math.random().toString(36).substr(2, 15);
+	})
+	.add('createTimeStamp', function() {
+		return parseInt(new Date().getTime() / 1000) + '';
+	})
+	.add('sign', function(options) {
+		var ops = options || {};
+		var
+			keys =
+			Object.keys(ops)
+			.filter(function(item) {
+				return ops[item] !== undefined && ops[item] !== '' && ['pfx', 'partner_key', 'sign', 'key'].indexOf(item) < 0;
+			})
+			.sort()
+			.map(function(key) {
+				return key + '=' + ops[key];
+			})
+			.join('&') + '&key=' + this.partner_key;
+		return md5(keys).toUpperCase();
 	});
 
-};
-var wxPay = {
-	createNonceStr: createNonceStr,
-	createTimeStamp: createTimeStamp,
-	Unifiedorder: Unifiedorder
-};
+/**
+ * @description 微信小程序支付
+ * @constructor WeixinPay
+ * @author kingwell
+ * @Date(2016-11-22)
+ */
+function WeiXinPay() {
+	this.init.apply(this, arguments);
+}
 
-module.exports = wxPay;
+WeiXinPay
+//继承 
+	.inherits(new Utils(), new WXPayUrl())
+	/**
+	 * @description init
+	 */
+	.add('init', function() {
+		for (var key in arguments[0]) {
+			this[key] = arguments[0][key];
+		}
+	})
+	/**
+	 * 创建统一订单
+	 * @param  {[type]} options       [description]
+	 * @param  {[type]} fn)           {		var       that  [description]
+	 * @param  {[type]} }            [description]
+	 * @param  {[type]} function(err, response,     body) {			console.log('body', body);			parseString(body, function(err, result) {				fn(that.parseWXReturnXML(result));			} [description]
+	 * @return {[type]}               [description]
+	 */
+	.add('createUnifiedOrder', function(param, fn) {
+		var that = this;
+		var ops = param || {};
+		var nonce_str = that.createNonceStr();
+
+		ops.appid = that.appid;
+		ops.openid = that.openid;
+		ops.mch_id = that.mch_id;
+		ops.nonce_str = nonce_str;
+		ops.sign = that.sign(ops);
+		request({
+			url: that.url.unifiedorder,
+			method: 'POST',
+			body: builder.buildObject(ops),
+			agentOptions: {
+				pfx: that.pfx,
+				passphrase: that.mch_id
+			}
+		}, function(err, response, body) {
+			parseString(body, function(err, result) {
+				fn(that.parseWXReturnXML(result));
+			});
+		});
+		return that;
+	})
+	/**
+	 * @description 查寻订单
+	 * @param  {Object} param
+	 * @param  {Function} fn) callback
+	 * @return {Object} Constructor
+	 */
+	.add('queryOrder', function(param, fn) {
+		var that = this;
+		param.nonce_str = param.nonce_str || that.createNonceStr();
+
+		param.appid = that.appid;
+		param.mch_id = that.mch_id;
+		param.sign = that.sign(param);
+
+		request({
+			url: that.url.orderquery,
+			method: 'POST',
+			body: builder.buildObject(param)
+		}, function(err, response, body) {
+			parseString(body, function(err, result) {
+				fn(that.parseWXReturnXML(result));
+			});
+		});
+		return that;
+	})
+	/**
+	 * @description 关闭订单
+	 * @param  {Object} param
+	 * @param  {Function} fn) callback
+	 * @return {Object} Constructor
+	 */
+	.add('closeorder', function(param, fn) {
+		var that = this;
+		param.appid = this.appid;
+		param.mch_id = this.mch_id;
+		param.nonce_str = param.nonce_str || that.createNonceStr();
+		param.sign = that.sign(param);
+		request({
+			url: this.url.closeorder,
+			method: 'POST',
+			body: builder.buildObject(param)
+		}, function(err, response, body) {
+			parseString(body, function(err, result) {
+				fn(that.parseWXReturnXML(result));
+			});
+		});
+
+	});
+
+module.exports = WeiXinPay;
